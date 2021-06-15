c0938fc8b115ab8e495189abbc864ae88343e52c
simransindhwani <simransindhwani2@gmail.com>
2021-06-15 11:04:08 -0500

    Added models

--- src/main/java/com/egen/JPAConfig.java
+++ src/main/java/com/egen/JPAConfig.java
@@ -1,8 +1,12 @@
 package com.egen;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -14,26 +18,35 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class JPAConfig {
 
-//	@Bean
+	@Bean
 	public LocalContainerEntityManagerFactoryBean emf() {
-		//TODO: configure emf
-		return null;
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(dataSource());
+		emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		emf.setPackagesToScan("com.egen.model");
+
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL57Dialect");
+		properties.put("hibernate.hbm2ddl.auto","create");
+		properties.put("hibernate.show_sql", "true");
+		emf.setJpaProperties(properties);
+		return emf;
 	}
 
-//	@Bean
+	@Bean
 	public DataSource dataSource() {
-		//TODO: configure data source bean
-		return  null;
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/order_db?useSSL=false");
+		ds.setUsername("root");
+		ds.setPassword("");
+		return ds;
 	}
 
-//	@Bean
+	@Bean
 	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-		//TODO: configure transaction manager
-		return null;
+		JpaTransactionManager txm = new JpaTransactionManager(emf);
+		return txm;
 	}
 
-	private Properties jpaProperties() {
-		//TODO: configure jpa properties
-		return null;
-	}
 }
